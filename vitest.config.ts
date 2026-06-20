5d798c662f42f3a48e63690c3114a356d6a174b5
CherryXiao <FKUfeng01@gmail.com>
2026-06-20 08:00:48 +0800

    fix: remove unnecessary vitest cache override

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  cacheDir: ".vitest",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
